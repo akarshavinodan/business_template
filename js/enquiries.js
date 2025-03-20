8eddf00c425b873aeb9b5224628bd095621fd7a6
Akarsha-git <adwaithvinodan414@gmail.com>
2025-03-20 12:28:46 +0530

    enquiries section added seperate css and js files

---
+++ js/enquiries.js
@@ -0,0 +1,20 @@
+  // Sidebar Toggle
+  const sidebar = document.getElementById('sidebar');
+  const mainContent = document.getElementById('main-content');
+  const toggleBtn = document.getElementById('toggle-btn');
+
+  toggleBtn.addEventListener('click', () => {
+      if (window.innerWidth <= 991.98) {
+          sidebar.classList.toggle('expanded');
+      } else {
+          sidebar.classList.toggle('collapsed');
+          mainContent.classList.toggle('expanded');
+      }
+  });
+
+  // Close sidebar on window resize
+  window.addEventListener('resize', () => {
+      if (window.innerWidth > 991.98) {
+          sidebar.classList.remove('expanded');
+      }
+  });
