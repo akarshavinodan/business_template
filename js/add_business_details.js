6d7bb9bb8f46fc075e1c2dfa5d9575eb24d9845e
Akarsha-git <adwaithvinodan414@gmail.com>
2025-03-21 13:21:27 +0530

    linked the logo with home page in each pages of user dashboard

--- js/add_business_details.js
+++ js/add_business_details.js
@@ -1,3 +1,27 @@
+const sidebar = document.getElementById('sidebar');
+const mainContent = document.getElementById('main-content');
+const toggleBtn = document.getElementById('toggle-btn');
+
+toggleBtn.addEventListener('click', () => {
+    if (window.innerWidth <= 991.98) {
+        sidebar.classList.toggle('expanded');
+    } else {
+        sidebar.classList.toggle('collapsed');
+        mainContent.classList.toggle('expanded');
+    }
+});
+
+// Close sidebar on window resize
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 991.98) {
+        sidebar.classList.remove('expanded');
+    }
+});
+
+
+
+
+
 function openEditModal() {
     const modal = new bootstrap.Modal(document.getElementById('editBusinessModal'));
     modal.show();
