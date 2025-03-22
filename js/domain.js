3fb39b97a793a29dfb6615bf652881ef8c9d3ffd
Akarsha-git <adwaithvinodan414@gmail.com>
2025-03-22 11:14:15 +0530

    domain page changes

--- js/domain.js
+++ js/domain.js
@@ -22,6 +22,19 @@ window.addEventListener('resize', () => {
 
 
 function showDnsInstructions() {
-    const modal = new bootstrap.Modal(document.getElementById('dnsInstructionsModal'));
-    modal.show();
+    document.getElementById('domainCard').style.display = 'none';
+    document.getElementById('dnsCard').style.display = 'block';
 }
+
+function showDomainCard() {
+    document.getElementById('dnsCard').style.display = 'none';
+    document.getElementById('domainCard').style.display = 'block';
+}
+
+function showHostingConfirmation() {
+    document.getElementById('dnsCard').style.display = 'none';
+    document.getElementById('hostingCard').style.display = 'block';
+}
+
+const modal = new bootstrap.Modal(document.getElementById('dnsInstructionsModal'));
+modal.show();
