07be8b85b26272220f7bbb5efb584cba284acceb
Akarsha-git <adwaithvinodan414@gmail.com>
2025-03-22 10:16:51 +0530

    domain part added

---
+++ js/domain.js
@@ -0,0 +1,27 @@
+const sidebar = document.getElementById('sidebar');
+const mainContent = document.getElementById('main-content');
+const toggleBtn = document.getElementById('toggle-btn');
+
+toggleBtn.addEventListener('click', () => {
+    if (window.innerWidth <= 991.98) {
+        sidebar.classList.toggle('expanded');
+    } else {
+        sidebar.classList.toggle('collapsed');
+        mainContent.classList.toggle('expanded');
+    }
+});
+
+// Close sidebar on window resize
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 991.98) {
+        sidebar.classList.remove('expanded');
+    }
+});
+
+
+
+
+function showDnsInstructions() {
+    const modal = new bootstrap.Modal(document.getElementById('dnsInstructionsModal'));
+    modal.show();
+}
